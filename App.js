d917d4bbd815e0d71f4ccd0940dedb31734415d6
shilpi verma <sverma@nobul.com>
2023-12-26 17:58:00 +0530

    Js vs JSx

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
-const heading = React.createElement(
-  "h1",
-  { id: "heading", className: "heading-h1" },
-  "Hello world from react!"
-);
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent", className: "parent-div" },
-  [
-    React.createElement("div", { id: "child1", className: "child-div1" }, [
-      React.createElement("h1", { id: "h1" }, "I am heading-h1"),
-      React.createElement("h2", { id: "h2" }, "I am heading-h2"),
-    ]),
-    React.createElement("div", { id: "child2", className: "child-div2" }, [
-      React.createElement("h3", { id: "h3" }, "I am heading-h3"),
-      React.createElement("h3", { id: "h3" }, "I am heading-h3"),
-    ]),
-  ]
-);
-console.log(parent);
-
+const h1 = React.createElement("h1", { id: "h1-heading" }, "My react app");
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(parent); //converts this heading object into an h1 tag and put in into the dom.
+
+const jsxHeading = <h1>This is a jsx heading</h1>;
+root.render(jsxHeading);
+console.log(jsxHeading);
+
+const htmlheading = document.createElement("h1");
+htmlheading.innerHTML = "This is a html heading";
+console.log(htmlheading);
