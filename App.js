52bf1ec9ba7bf05a1fd3e7358fd7263dc11e4b50
shilpi verma <sverma@nobul.com>
2023-12-26 19:26:22 +0530

    JSX handlex cross script attacks

--- App.js
+++ App.js
@@ -3,19 +3,29 @@ import ReactDOM from "react-dom/client";
 
 //JSX =>Babel transpiles to React.createElement => React Element(JS Object) => HTML Element (on render)
 
-const jsxHeading = () => <h1>This is a jsx heading</h1>;
+const JsxHeading = () => <h1>This is a jsx heading</h1>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-console.log(jsxHeading);
+const title = <h1 className="h1">Namaste React</h1>;
 
+console.log(JsxHeading);
+const number = 100;
 //React Functional Component
 //Component Composition
+
+//CrossScript attack
+const data = abc.getData();
+// JSX will take care of any cross script attack by santisizing the code
 const HeadingComponent = () => {
-  <div id="container">
-    <jsxHeading />
-    <h1 className="heading">This is a jsx elemnt</h1>
-  </div>;
-  return <h1>this return a react element</h1>;
+  return (
+    <div id="container">
+      <JsxHeading />
+      {data}
+      {title}
+      {number}
+      <h1 className="heading">This is a jsx elemnt</h1>
+    </div>
+  );
 };
 
 console.log(HeadingComponent);
